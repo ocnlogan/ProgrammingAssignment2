571d5ec5990431f4c629396c75fa48c573c8040a
ocnLogan <ocnlogan@outlook.com>
2015-06-18 18:14:18 -0700

    Changed my version of the file, seems to work as intended. Attempting to commit to github now.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+#this function is similar to the example function given.
+#It accepts matrix variables
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function() x
+     setmatrix <- function(solve) m <<- solve
+     getmatrix <- function() m
+     list(set = set, get = get,
+          setmatrix = setmatrix,
+          getmatrix = getmatrix)
 }
 
 
 ## Write a short comment describing this function
+# The function is modified from the example function provided.
+# It solves the inverse of a matrix given in the variable
+# and caches it. If the inverse has already been calculated
+# the function returns the cached copy.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     
+     m <- x$getmatrix()
+     if(!is.null(m)) {
+          message("getting cached inverse matrix")
+          return(m)
+     }
+     matrix <- x$get()
+     m <- solve(matrix, ...)
+     x$setmatrix(m)
+     m
 }
